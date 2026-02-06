ea1ee644cfc488dd360a731b8a5e0a5a62acffd5
iDevorei <messewalter@gmail.com>
2026-02-06 20:01:57 +0300

    feat: add code to lists.py

---
+++ lists.py
@@ -0,0 +1,25 @@
+# *Создайте список и заполните его строками 'ab', 'cd', 'ef'.
+
+lst = ['ab', 'cd', 'ef']
+
+# *списки в python это почти тоже, что и массивы в JavaScript
+
+# Создайте список, состоящий из названий дней недели.
+
+lst_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+# Добавьте к списку, созданному в предыдущем задании, цифры соответствующие дням недели.
+
+lst_week = ['Monday', 1, 'Tuesday', 2, 'Wednesday', 3, 'Thursday', 4, 'Friday', 5, 'Saturday', 6, 'Sunday', 7]
+
+lst = list('1234')
+
+# !числа вводить нельзя!
+
+print(lst)
+
+txt = '1-2-3-4'
+
+print(txt.split('-'))
+
+# !Получение отдельного элемента списка в Python
